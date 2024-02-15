db9e8eb694d29be104840c55ad63c50b497008ca
Sergio Afonso <safonsof@amd.com>
2024-02-15 12:55:22 +0000

    Fix failing unit tests

--- flang/test/Lower/OpenMP/Todo/reduction-teams.f90
+++ flang/test/Lower/OpenMP/Todo/reduction-teams.f90
@@ -1,7 +1,9 @@
-! RUN: %not_todo_cmd bbc -emit-fir -fopenmp -o - %s 2>&1 | FileCheck %s
-! RUN: %not_todo_cmd %flang_fc1 -emit-fir -fopenmp -o - %s 2>&1 | FileCheck %s
+! RUN: bbc -emit-fir -fopenmp -o - %s | FileCheck %s
+! RUN: %flang_fc1 -emit-fir -fopenmp -o - %s | FileCheck %s
+! XFAIL: *
 
-! CHECK: 'omp.reduction' op the accumulator is not used by the parent
+! CHECK:       omp.teams
+! CHECK-SAME:  reduction
 subroutine reduction_teams()
   integer :: i
   i = 0
