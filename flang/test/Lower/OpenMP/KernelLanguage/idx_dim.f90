2b2907b63cafa31005e54923f5b54c1d4c8d90d4
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2024-09-25 16:48:56 +0900

    Test file

---
+++ flang/test/Lower/OpenMP/KernelLanguage/idx_dim.f90
@@ -0,0 +1,26 @@
+! RUN: %flang_fc1 -emit-hlfir -fopenmp %s -o - | FileCheck %s
+
+module mod1
+contains
+  subroutine sub1()
+    use omp_lib
+    integer :: tmp
+    tmp = ompx_thread_id_x()
+    tmp = ompx_thread_id_y()
+    tmp = ompx_thread_id_z()
+    tmp = ompx_block_id_x()
+    tmp = ompx_block_id_y()
+    tmp = ompx_block_id_z()
+    tmp = ompx_block_dim_x()
+    tmp = ompx_block_dim_y()
+    tmp = ompx_block_dim_z()
+    tmp = ompx_grid_dim_x()
+    tmp = ompx_grid_dim_y()
+    tmp = ompx_grid_dim_z()
+  end subroutine sub1
+
+  subroutine host()
+    !$ omp target teams ompx_bare num_teams(1, 2, 3) thread_limit(4, 5, 6)
+    call sub1()
+  end subroutine host
+end module
